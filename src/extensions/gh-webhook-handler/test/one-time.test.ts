6905973cf69d3b10b5f3a289694f182dbc43e544
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-09-25 13:21:45 +0200

    refactor: move from bun to mocha

--- src/extensions/gh-webhook-handler/test/one-time.test.ts
+++ src/extensions/gh-webhook-handler/test/one-time.test.ts
@@ -1,7 +1,8 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
 import { OperationContext } from '@directus/types';
-import { test, expect, mock, beforeEach } from 'bun:test';
 import operationApi from '../src/api.js';
-import oneTimeSponsorshipCreated from './one-time-sonsorship-created.json';
+import oneTimeSponsorshipCreated from './one-time-sonsorship-created.json' assert { type: 'json' };
 
 const database = {} as OperationContext['database'];
 const accountability = {} as OperationContext['accountability'];
@@ -11,109 +12,111 @@ const env = {
 	GITHUB_WEBHOOK_TOKEN: '77a9a254554d458f5025bb38ad1648a3bb5795e8',
 	CREDITS_PER_DOLLAR: '10000'
 };
-const createOne = mock(() => Promise.resolve(1));
+const createOne = sinon.stub().resolves(1);
 const services = {
-	ItemsService: mock(function () { return { createOne }}),
+	ItemsService: sinon.stub().returns({ createOne }),
 };
 
 beforeEach(() => {
-	createOne.mockClear();
-	services.ItemsService.mockClear();
+	sinon.resetHistory();
 });
 
-test('gh-webhook-handler should handle valid one-time sponsorship', async () => {
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=005bb451b83a393675d01ae33e2f778c2c245b4093d46702ad15917717384c9b'
-			},
-			body: oneTimeSponsorshipCreated
-		}
-	};
+describe('GitHub webhook one-time handler', () => {
+	it('should handle valid one-time sponsorship', async () => {
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=005bb451b83a393675d01ae33e2f778c2c245b4093d46702ad15917717384c9b'
+				},
+				body: oneTimeSponsorshipCreated
+			}
+		};
 
-	const result = await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability });
+		const result = await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability });
 
-	expect(services.ItemsService).toHaveBeenCalledTimes(1);
-	expect(services.ItemsService.mock.calls[0]).toEqual([ 'credits', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(createOne).toHaveBeenCalledTimes(1);
-	expect(createOne.mock.calls[0]).toEqual([{
-		githubLogin: 'monalisa',
-		githubId: '2',
-		credits: 50000,
-		comment: 'For 5$ sponsorship'
-	}]);
-	expect(result).toEqual('Credits item with id: 1 created. One-time sponsorship handled.');
-});
+		expect(services.ItemsService.callCount).to.equal(1);
+		expect(services.ItemsService.args[0]).to.deep.equal([ 'credits', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(createOne.callCount).to.equal(1);
+		expect(createOne.args[0]).to.deep.equal([{
+			githubLogin: 'monalisa',
+			githubId: '2',
+			credits: 50000,
+			comment: 'For 5$ sponsorship'
+		}]);
+		expect(result).to.equal('Credits item with id: 1 created. One-time sponsorship handled.');
+	});
 
-test('gh-webhook-handler should throw without GITHUB_WEBHOOK_TOKEN env', async () => {
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=005bb451b83a393675d01ae33e2f778c2c245b4093d46702ad15917717384c9b'
-			},
-			body: oneTimeSponsorshipCreated
-		}
-	};
-	const env = {};
+	it('should throw without GITHUB_WEBHOOK_TOKEN env', async () => {
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=005bb451b83a393675d01ae33e2f778c2c245b4093d46702ad15917717384c9b'
+				},
+				body: oneTimeSponsorshipCreated
+			}
+		};
+		const env = {};
 
-	await expect(async () => await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }))
-		.toThrow(new Error('GITHUB_WEBHOOK_TOKEN was not provided'));
-	expect(createOne).toHaveBeenCalledTimes(0);
-});
+		const err = await (operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }) as Promise<string>).catch(err => err);
+		expect(err).to.deep.equal(new Error('GITHUB_WEBHOOK_TOKEN was not provided'));
+		expect(createOne.callCount).to.equal(0);
+	});
 
-test('gh-webhook-handler should throw without x-hub-signature-256 header', async () => {
-	const data = {
-		$trigger: {
-			headers: {},
-			body: oneTimeSponsorshipCreated
-		}
-	};
+	it('should throw without x-hub-signature-256 header', async () => {
+		const data = {
+			$trigger: {
+				headers: {},
+				body: oneTimeSponsorshipCreated
+			}
+		};
 
-	await expect(async () => await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }))
-		.toThrow(new Error('"x-hub-signature-256" header was not provided'));
-	expect(services.ItemsService).toHaveBeenCalledTimes(0);
-	expect(createOne).toHaveBeenCalledTimes(0);
-});
+		const err = await (operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }) as Promise<string>).catch(err => err);
+		expect(err).to.deep.equal(new Error('"x-hub-signature-256" header was not provided'));
+		expect(services.ItemsService.callCount).to.equal(0);
+		expect(createOne.callCount).to.equal(0);
+	});
 
-test('gh-webhook-handler should throw with wrong x-hub-signature-256 header', async () => {
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=wrongSignatureValueWrongSignatureValueWrongSignatureValueWrongSi'
-			},
-			body: oneTimeSponsorshipCreated
-		}
-	};
+	it('should throw with wrong x-hub-signature-256 header', async () => {
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=wrongSignatureValueWrongSignatureValueWrongSignatureValueWrongSi'
+				},
+				body: oneTimeSponsorshipCreated
+			}
+		};
 
-	await expect(async () => await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }))
-		.toThrow(new Error('Signature is not valid'));
-	expect(services.ItemsService).toHaveBeenCalledTimes(0);
-	expect(createOne).toHaveBeenCalledTimes(0);
-});
+		const err = await (operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }) as Promise<string>)
+			.catch(err => err);
+		expect(err).to.deep.equal(new Error('Signature is not valid'));
+		expect(services.ItemsService.callCount).to.equal(0);
+		expect(createOne.callCount).to.equal(0);
+	});
 
-test('gh-webhook-handler should throw without sponsor field in sponsorship object', async () => {
-	const {action, sponsorship, sender} = oneTimeSponsorshipCreated;
-	const {sponsor, ...otherSponsorshipFields} = sponsorship;
+	it('should throw without sponsor field in sponsorship object', async () => {
+		const {action, sponsorship, sender} = oneTimeSponsorshipCreated;
+		const {sponsor, ...otherSponsorshipFields} = sponsorship;
 
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=d58d9027d97211d8378194a43ca5ca2c6ee3ed5e7afcdabb2ce373048f9f9acc'
-			},
-			body: {
-				action,
-				sender,
-				sponsorship: {
-					...otherSponsorshipFields
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=d58d9027d97211d8378194a43ca5ca2c6ee3ed5e7afcdabb2ce373048f9f9acc'
+				},
+				body: {
+					action,
+					sender,
+					sponsorship: {
+						...otherSponsorshipFields
+					}
 				}
 			}
-		}
-	};
+		};
 
-	await expect(async () => await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }))
-		.toThrow(new Error('"sponsorship.sponsor" field is undefined'));
-	expect(createOne).toHaveBeenCalledTimes(0);
+		const err = await (operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability }) as Promise<string>).catch(err => err);
+		expect(err).to.deep.equal(new Error('"sponsorship.sponsor" field is undefined'));
+		expect(createOne.callCount).to.equal(0);
+	});
 });
--- src/extensions/gh-webhook-handler/test/recurring.test.ts
+++ src/extensions/gh-webhook-handler/test/recurring.test.ts
@@ -1,12 +1,12 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
 import { OperationContext } from '@directus/types';
-import { test, expect, mock, beforeEach, beforeAll, setSystemTime } from 'bun:test';
 import _ from 'lodash';
 import operationApi from '../src/api.js';
-import recurringSponsorshipCreated from './recurring-sponsorship-created.json';
-import recurringSponsorshipTierChanged from './recurring-sponsorship-tier-changed.json';
+import recurringSponsorshipCreated from './recurring-sponsorship-created.json' assert { type: 'json' };
+import recurringSponsorshipTierChanged from './recurring-sponsorship-tier-changed.json' assert { type: 'json' };
 
 const database = {} as OperationContext['database'];
-const exceptions = {} as OperationContext['exceptions'];
 const accountability = {} as OperationContext['accountability'];
 const logger = (() => {}) as unknown as OperationContext['logger'];
 const getSchema = (() => Promise.resolve({})) as OperationContext['getSchema'];
@@ -14,11 +14,11 @@ const env = {
 	GITHUB_WEBHOOK_TOKEN: '77a9a254554d458f5025bb38ad1648a3bb5795e8',
 	CREDITS_PER_DOLLAR: '10000'
 };
-const creditsCreateOne = mock(() => Promise.resolve(1));
-const sponsorsCreateOne = mock(() => Promise.resolve(2));
-const sponsorsUpdateByQuery = mock(() => Promise.resolve(2));
+const creditsCreateOne = sinon.stub().resolves(1);
+const sponsorsCreateOne = sinon.stub().resolves(2);
+const sponsorsUpdateByQuery = sinon.stub().resolves(2);
 const services = {
-	ItemsService: mock(function (collection) {
+	ItemsService: sinon.stub().callsFake(function (collection) {
 		switch (collection) {
 			case 'credits':
 				return {createOne: creditsCreateOne};
@@ -30,122 +30,121 @@ const services = {
 	}),
 };
 
-beforeAll(() => {
-  setSystemTime(new Date("2023-09-19T00:00:00.000Z"));
+before(() => {
+	sinon.useFakeTimers(new Date('2023-09-19T00:00:00.000Z'));
 });
 
 beforeEach(() => {
-	creditsCreateOne.mockClear();
-	sponsorsCreateOne.mockClear();
-	sponsorsUpdateByQuery.mockClear();
-	services.ItemsService.mockClear();
+	sinon.resetHistory();
 });
 
-test('gh-webhook-handler should handle valid recurring sponsorship', async () => {
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=5715395b41f79dfc4850a380297bb9fa7addf83d969cb712f9a053ae190f2e17'
-			},
-			body: recurringSponsorshipCreated
-		}
-	};
-
-	const result = await operationApi.handler({}, { data, database, env, getSchema, services, exceptions, logger, accountability });
-
-	expect(services.ItemsService).toHaveBeenCalledTimes(2);
-	expect(services.ItemsService.mock.calls[0]).toEqual([ 'sponsors', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(services.ItemsService.mock.calls[1]).toEqual([ 'credits', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(creditsCreateOne).toHaveBeenCalledTimes(1);
-	expect(creditsCreateOne.mock.calls[0]).toEqual([{
-		githubLogin: 'monalisa',
-		githubId: '2',
-		credits: 150000,
-		comment: 'For 15$ sponsorship'
-	}]);
-	expect(sponsorsCreateOne).toHaveBeenCalledTimes(1);
-	expect(sponsorsCreateOne.mock.calls[0]).toEqual([{
-		githubLogin: 'monalisa',
-		githubId: '2',
-		monthlyAmount: 15,
-		lastEarningDate: '2023-09-19T00:00:00.000Z'
-	}]);
-	expect(result).toEqual('Sponsor with id: 2 created. Credits item with id: 1 created. Recurring sponsorship handled.');
-});
-
-test('gh-webhook-handler should handle tier change to the upper amount', async () => {
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=0b00755e53add3b61b5be9c5d2a237b59a9f6c9494721d46eae31f8fea06fdb9'
-			},
-			body: recurringSponsorshipTierChanged
-		}
-	};
-
-	const result = await operationApi.handler({}, { data, database, env, getSchema, services, exceptions, logger, accountability });
-
-	expect(services.ItemsService).toHaveBeenCalledTimes(2);
-	expect(services.ItemsService.mock.calls[0]).toEqual([ 'sponsors', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(services.ItemsService.mock.calls[1]).toEqual([ 'credits', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(sponsorsUpdateByQuery).toHaveBeenCalledTimes(1);
-	expect(sponsorsUpdateByQuery.mock.calls[0]).toEqual([{
-			filter: {
-				githubId: '2'
+describe('GitHub webhook recurring handler', () => {
+	it('should handle valid recurring sponsorship', async () => {
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=5715395b41f79dfc4850a380297bb9fa7addf83d969cb712f9a053ae190f2e17'
+				},
+				body: recurringSponsorshipCreated
 			}
-		}, {
-			monthlyAmount: 15
-		}
-	]);
-	expect(creditsCreateOne).toHaveBeenCalledTimes(1);
-	expect(creditsCreateOne.mock.calls[0]).toEqual([{
-		githubLogin: 'monalisa',
-		githubId: '2',
-		credits: 50000,
-		comment: 'For 5$ sponsorship'
-	}]);
-	expect(result).toEqual('Sponsor with id: 2 updated. Credits item with id: 1 created.');
-});
+		};
 
-test('gh-webhook-handler should handle tier change to the lower amount', async () => {
-	const body = _.cloneDeep(recurringSponsorshipTierChanged);
-	body.sponsorship.tier.monthly_price_in_dollars = 5;
-	const data = {
-		$trigger: {
-			headers: {
-				'x-hub-signature-256': 'sha256=cf9000279263c1e48b30afe5052317a830c102c268580983fadaa3a4bb21e7b1'
-			},
-			body
-		}
-	};
+		const result = await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability });
 
-	const result = await operationApi.handler({}, { data, database, env, getSchema, services, exceptions, logger, accountability });
+		expect(services.ItemsService.callCount).to.equal(2);
+		expect(services.ItemsService.args[0]).to.deep.equal([ 'sponsors', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(services.ItemsService.args[1]).to.deep.equal([ 'credits', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(creditsCreateOne.callCount).to.equal(1);
+		expect(creditsCreateOne.args[0]).to.deep.equal([{
+			githubLogin: 'monalisa',
+			githubId: '2',
+			credits: 150000,
+			comment: 'For 15$ sponsorship'
+		}]);
+		expect(sponsorsCreateOne.callCount).to.equal(1);
+		expect(sponsorsCreateOne.args[0]).to.deep.equal([{
+			githubLogin: 'monalisa',
+			githubId: '2',
+			monthlyAmount: 15,
+			lastEarningDate: '2023-09-19T00:00:00.000Z'
+		}]);
+		expect(result).to.equal('Sponsor with id: 2 created. Credits item with id: 1 created. Recurring sponsorship handled.');
+	});
 
-	expect(services.ItemsService).toHaveBeenCalledTimes(1);
-	expect(services.ItemsService.mock.calls[0]).toEqual([ 'sponsors', {
-		schema: {},
-		knex: {}
-	}]);
-	expect(sponsorsUpdateByQuery).toHaveBeenCalledTimes(1);
-	expect(sponsorsUpdateByQuery.mock.calls[0]).toEqual([{
-			filter: {
-				githubId: '2'
+	it('should handle tier change to the upper amount', async () => {
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=0b00755e53add3b61b5be9c5d2a237b59a9f6c9494721d46eae31f8fea06fdb9'
+				},
+				body: recurringSponsorshipTierChanged
 			}
-		}, {
-			monthlyAmount: 5
-		}
-	]);
-	expect(result).toEqual('Sponsor with id: 2 updated.');
+		};
+
+		const result = await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability });
+
+		expect(services.ItemsService.callCount).to.equal(2);
+		expect(services.ItemsService.args[0]).to.deep.equal([ 'sponsors', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(services.ItemsService.args[1]).to.deep.equal([ 'credits', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(sponsorsUpdateByQuery.callCount).to.equal(1);
+		expect(sponsorsUpdateByQuery.args[0]).to.deep.equal([{
+				filter: {
+					githubId: '2'
+				}
+			}, {
+				monthlyAmount: 15
+			}
+		]);
+		expect(creditsCreateOne.callCount).to.equal(1);
+		expect(creditsCreateOne.args[0]).to.deep.equal([{
+			githubLogin: 'monalisa',
+			githubId: '2',
+			credits: 50000,
+			comment: 'For 5$ sponsorship'
+		}]);
+		expect(result).to.equal('Sponsor with id: 2 updated. Credits item with id: 1 created.');
+	});
+
+	it('should handle tier change to the lower amount', async () => {
+		const body = _.cloneDeep(recurringSponsorshipTierChanged);
+		body.sponsorship.tier.monthly_price_in_dollars = 5;
+		const data = {
+			$trigger: {
+				headers: {
+					'x-hub-signature-256': 'sha256=cf9000279263c1e48b30afe5052317a830c102c268580983fadaa3a4bb21e7b1'
+				},
+				body
+			}
+		};
+
+		const result = await operationApi.handler({}, { data, database, env, getSchema, services, logger, accountability });
+
+		expect(services.ItemsService.callCount).to.equal(1);
+		expect(services.ItemsService.args[0]).to.deep.equal([ 'sponsors', {
+			schema: {},
+			knex: {}
+		}]);
+		expect(sponsorsUpdateByQuery.callCount).to.equal(1);
+		expect(sponsorsUpdateByQuery.args[0]).to.deep.equal([{
+				filter: {
+					githubId: '2'
+				}
+			}, {
+				monthlyAmount: 5
+			}
+		]);
+		expect(result).to.equal('Sponsor with id: 2 updated.');
+	});
 });
