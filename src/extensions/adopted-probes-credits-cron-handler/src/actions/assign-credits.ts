32d6d53a2c9895d08ebb21a84dfef29e2bc3ee47
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-12-15 15:15:56 +0100

    refactor: move required online times to the env var

--- src/extensions/adopted-probes-credits-cron-handler/src/actions/assign-credits.ts
+++ src/extensions/adopted-probes-credits-cron-handler/src/actions/assign-credits.ts
@@ -2,9 +2,15 @@ import type { OperationContext } from '@directus/extensions';
 import { getAdoptedProbes, addCredits, resetOnlineTimes } from '../repositories/directus';
 
 export const assignCredits = async (context: OperationContext) => {
+	const requiredOnlineTimes = context.env['ADOPTED_PROBES_REQUIRED_ONLINE_TIMES'];
+
+	if (!requiredOnlineTimes) {
+		throw new Error('ADOPTED_PROBES_REQUIRED_ONLINE_TIMES was not privided');
+	}
+
 	const adoptedProbes = await getAdoptedProbes(context);
 
-	const onlineAdoptedProbes = adoptedProbes.filter(({ onlineTimesToday }) => onlineTimesToday >= 10);
+	const onlineAdoptedProbes = adoptedProbes.filter(({ onlineTimesToday }) => onlineTimesToday >= requiredOnlineTimes);
 
 	const creditIds = await addCredits(onlineAdoptedProbes, context);
 	await resetOnlineTimes(context);
