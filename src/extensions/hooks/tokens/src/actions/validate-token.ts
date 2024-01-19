b4cde992fad49978f1fa48091891c1935f1481b5
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-01-19 15:00:26 +0100

    fix: add https:// prefix to token origins if required

--- src/extensions/hooks/tokens/src/actions/validate-token.ts
+++ src/extensions/hooks/tokens/src/actions/validate-token.ts
@@ -1,8 +1,25 @@
-import Joi, { type CustomHelpers } from 'joi';
+import Joi, { ValidationError, type CustomHelpers } from 'joi';
 import { createError } from '@directus/errors';
 import { Token } from '../index.js';
 
+function findProtocolSymbol (str: string) {
+	// Find '://', ':/', and ':' before the first '.'
+	const firstPart = str.split('.')[0]!;
+	const regex = /(:\/\/|:\/|:)/;
+
+	const match = firstPart.match(regex);
+	return match ? match[0] : null;
+}
+
 const validateOrigin = (value: string, helpers: CustomHelpers) => {
+	const protocolSymbol = findProtocolSymbol(value);
+
+	if (protocolSymbol) {
+		value = value.replace(protocolSymbol, '://');
+	} else if (protocolSymbol === null) {
+		value = 'https://' + value;
+	}
+
 	try {
 		new URL(value);
 		return value;
@@ -16,9 +33,11 @@ const tokenSchema = Joi.object({
 }).unknown(true);
 
 export const validateToken = (token: Partial<Token>) => {
-	const { error } = tokenSchema.validate(token);
+	const { value, error } = tokenSchema.validate(token) as { value: Partial<Token>, error: ValidationError };
 
 	if (error) {
 		throw new (createError('INVALID_PAYLOAD_ERROR', error.message, 400))();
 	}
+
+	Object.assign(token, value);
 };
