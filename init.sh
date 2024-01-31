051177fdb199ad220e5b1449d1371acac98e25e8
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-01-31 17:03:05 +0100

    feat: add dev mode to init

--- init.sh
+++ init.sh
@@ -25,20 +25,42 @@ function get_token {
   echo "$token"
 }
 
+is_dev_mode=false
+
+for arg in "$@"
+do
+    if [ "$arg" = "--dev" ]; then
+        is_dev_mode=true
+        break
+    fi
+done
+
 token=$(get_token)
 
 perl -pi -e "s/ADMIN_ACCESS_TOKEN=.*/ADMIN_ACCESS_TOKEN=$token/" .env
 
-npm run migrate
-
 npm run schema:apply
+
+npm run migrate
 
 user_role_id=$(curl -H "Authorization: Bearer $token" $DIRECTUS_URL/roles | jq -r '.data[] | select(.name == "User") | .id')
 
-confirm "Set that value to the container env vars: \nAUTH_GITHUB_DEFAULT_ROLE_ID=$user_role_id \nThen restart the container." # Restart is requred to apply new role id and because of https://github.com/directus/directus/issues/17117
+if [ "$is_dev_mode" = true ]; then
+	echo "user_role_id=$user_role_id"
 
-confirm "Login using github. Re-login as admin and give github user admin rights. Then set that value to the container env vars: \nAUTH_DISABLE_DEFAULT=true \nThen restart the container."
+	perl -pi -e "s/AUTH_GITHUB_DEFAULT_ROLE_ID=.*/AUTH_GITHUB_DEFAULT_ROLE_ID=$user_role_id/" .env.development
 
-confirm "Login using github. Generate a static access token for your user and save it the local .env file as ADMIN_ACCESS_TOKEN"
+	docker compose stop directus
+
+	docker compose up -d directus
+
+	./wait-for.sh -t 60 http://localhost:8055/admin/login
+else
+	confirm "Set that value to the container env vars: \nAUTH_GITHUB_DEFAULT_ROLE_ID=$user_role_id \nThen restart the container." # Restart is requred to apply new role id and because of https://github.com/directus/directus/issues/17117
+
+	confirm "Login using github. Re-login as admin and give github user admin rights. Then set that value to the container env vars: \nAUTH_DISABLE_DEFAULT=true \nThen restart the container."
+
+	confirm "Login using github. Generate a static access token for your user and save it the local .env file as ADMIN_ACCESS_TOKEN"
+fi
 
 echo "Finished"
---
+++ src/extensions/migrations/20240131A-add-credits-triggers.js
@@ -0,0 +1,44 @@
+export async function up (knex) {
+	await knex.transaction(async (trx) => {
+		await trx.raw(`
+			CREATE TRIGGER after_gp_credits_additions_insert
+			AFTER INSERT ON gp_credits_additions
+			FOR EACH ROW
+			BEGIN
+					DECLARE found_user_id CHAR(36);
+					SELECT id INTO found_user_id FROM directus_users WHERE external_identifier = NEW.githubId LIMIT 1;
+
+					IF found_user_id IS NOT NULL THEN
+							INSERT INTO gp_credits (user_id, amount)
+							VALUES (found_user_id, NEW.amount)
+							ON DUPLICATE KEY UPDATE
+							gp_credits.amount = gp_credits.amount + NEW.amount;
+					END IF;
+			END;
+		`);
+
+		await trx.raw(`
+			CREATE TRIGGER after_gp_credits_update
+			AFTER UPDATE ON gp_credits
+			FOR EACH ROW
+			BEGIN
+					IF NEW.amount < OLD.amount THEN
+							UPDATE gp_credits_deductions
+							SET amount = amount + (OLD.amount - NEW.amount)
+							WHERE user_id = NEW.user_id AND date = CURRENT_DATE;
+
+							IF ROW_COUNT() = 0 THEN
+									INSERT INTO gp_credits_deductions (user_id, amount, date)
+									VALUES (NEW.user_id, OLD.amount - NEW.amount, CURRENT_DATE);
+							END IF;
+					END IF;
+			END;
+		`);
+	});
+
+	console.log('Triggers for gp_credits_additions and gp_credits created');
+}
+
+export async function down () {
+	console.log('There is no down operation for that migration.');
+}
