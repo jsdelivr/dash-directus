d60d622e9fdfb8fa8fb3638875c62d5b08fbd241
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-02-05 14:10:09 +0100

    feat: remove non-used transactions

--- src/extensions/migrations/20240131A-add-credits-triggers.js
+++ src/extensions/migrations/20240131A-add-credits-triggers.js
@@ -1,57 +1,62 @@
 export async function up (knex) {
-	await knex.transaction(async (trx) => {
-		await trx.raw(`
-			CREATE TRIGGER after_gp_credits_additions_insert
-			BEFORE INSERT ON gp_credits_additions
-			FOR EACH ROW
-			BEGIN
-					DECLARE found_user_id CHAR(36);
-					SELECT id INTO found_user_id FROM directus_users WHERE external_identifier = NEW.github_id LIMIT 1;
+	await knex.raw(`
+		CREATE TRIGGER after_gp_credits_additions_insert
+		BEFORE INSERT ON gp_credits_additions
+		FOR EACH ROW
+		BEGIN
+				DECLARE found_user_id CHAR(36);
+				SELECT id INTO found_user_id FROM directus_users WHERE external_identifier = NEW.github_id LIMIT 1;
 
-					IF found_user_id IS NOT NULL THEN
-							INSERT INTO gp_credits (user_id, amount, date_created)
-							VALUES (found_user_id, NEW.amount, CURRENT_TIMESTAMP)
-							ON DUPLICATE KEY UPDATE
-							gp_credits.amount = gp_credits.amount + NEW.amount;
-					ELSE
-							SET NEW.consumed = FALSE;
-					END IF;
-			END;
-		`);
+				IF found_user_id IS NOT NULL THEN
+						INSERT INTO gp_credits (user_id, amount, date_created)
+						VALUES (found_user_id, NEW.amount, CURRENT_TIMESTAMP)
+						ON DUPLICATE KEY UPDATE
+						gp_credits.amount = COALESCE(gp_credits.amount, 0) + NEW.amount;
+				ELSE
+						SET NEW.consumed = FALSE;
+				END IF;
+		END;
+	`);
 
-		await trx.raw(`
-			ALTER TABLE gp_credits
-			ADD CONSTRAINT gp_credits_amount_positive CHECK (amount >= 0);
-		`);
+	await knex.raw(`
+		ALTER TABLE gp_credits
+		ADD CONSTRAINT gp_credits_amount_positive CHECK (amount >= 0);
+	`);
 
-		await trx.raw(`
-			ALTER TABLE gp_credits_additions
-			ADD CONSTRAINT gp_credits_additions_amount_positive CHECK (amount >= 0);
-		`);
+	await knex.raw(`
+		ALTER TABLE gp_credits_additions
+		ADD CONSTRAINT gp_credits_additions_amount_positive CHECK (amount >= 0);
+	`);
 
-		await trx.raw(`
-			ALTER TABLE gp_credits_deductions
-			ADD CONSTRAINT gp_credits_deductions_amount_positive CHECK (amount >= 0);
-		`);
+	await knex.raw(`
+		ALTER TABLE gp_credits_deductions
+		ADD CONSTRAINT gp_credits_deductions_amount_positive CHECK (amount >= 0);
+	`);
 
-		await trx.raw(`
-			CREATE TRIGGER after_gp_credits_update
-			AFTER UPDATE ON gp_credits
-			FOR EACH ROW
-			BEGIN
-					IF NEW.amount < OLD.amount THEN
-							UPDATE gp_credits_deductions
-							SET amount = amount + (OLD.amount - NEW.amount)
-							WHERE user_id = NEW.user_id AND date = CURRENT_DATE;
+	await knex.raw(`
+		ALTER TABLE gp_credits_deductions
+		ADD CONSTRAINT unique_user_id_date UNIQUE (user_id, date);
+	`);
 
-							IF ROW_COUNT() = 0 THEN
-									INSERT INTO gp_credits_deductions (user_id, amount, date)
-									VALUES (NEW.user_id, OLD.amount - NEW.amount, CURRENT_DATE);
-							END IF;
-					END IF;
-			END;
-		`);
-	});
+	await knex.raw(`
+		CREATE TRIGGER after_gp_credits_update
+		AFTER UPDATE ON gp_credits
+		FOR EACH ROW
+		BEGIN
+				IF NEW.amount < OLD.amount THEN
+						UPDATE gp_credits_deductions
+						SET amount = amount + (OLD.amount - NEW.amount)
+						WHERE user_id = NEW.user_id AND date = CURRENT_DATE;
+
+						IF ROW_COUNT() = 0 THEN
+								INSERT INTO gp_credits_deductions (user_id, amount, date)
+								VALUES (NEW.user_id, OLD.amount - NEW.amount, CURRENT_DATE)
+								ON DUPLICATE KEY UPDATE
+								amount = amount + (OLD.amount - NEW.amount);
+						END IF;
+				END IF;
+		END;
+	`);
 
 	console.log('Triggers for gp_credits_additions and gp_credits created');
 }
--- src/extensions/migrations/20240202A-add-indexes.js
+++ src/extensions/migrations/20240202A-add-indexes.js
@@ -1,11 +1,9 @@
 export async function up (knex) {
-	await knex.transaction(async (trx) => {
-		await trx.raw(`ALTER TABLE jsd_purge_tokens ADD INDEX value_index (value);`);
-		await trx.raw(`ALTER TABLE gp_tokens ADD INDEX value_index (value);`);
-		await trx.raw(`ALTER TABLE gp_adopted_probes ADD INDEX ip_index (ip);`);
-		await trx.raw(`ALTER TABLE gp_credits ADD INDEX user_id_index (user_id);`);
-		await trx.raw(`ALTER TABLE gp_credits_deductions ADD INDEX user_id_and_date_index (user_id, date);`);
-	});
+	await knex.raw(`ALTER TABLE jsd_purge_tokens ADD INDEX value_index (value);`);
+	await knex.raw(`ALTER TABLE gp_tokens ADD INDEX value_index (value);`);
+	await knex.raw(`ALTER TABLE gp_adopted_probes ADD INDEX ip_index (ip);`);
+	await knex.raw(`ALTER TABLE gp_credits ADD INDEX user_id_index (user_id);`);
+	await knex.raw(`ALTER TABLE gp_credits_deductions ADD INDEX user_id_and_date_index (user_id, date);`);
 
 	console.log('Indexes added');
 }
